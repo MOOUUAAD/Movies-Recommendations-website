d77373f6a660847a898366801e62154bf30cd57c
MOOUUAAD <mouad.elmaslouhi.2003@gmail.com>
2024-04-27 14:17:46 +0100

    last up

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -7,13 +7,13 @@ const Card = (movie) => {
     ev.target.src = "https://timescineplex.com/times/img/no-poster.png"
  }
   let image_path ="https://image.tmdb.org/t/p/w500"
-  console.log(movie.info);
+
   return (
-    <div className='w-1/2 md:w-1/5 m-4 relative bg-sky-950 overflow-y-hidden shadow-md shadow-black hover:scale-105 duration-200 rounded'>
+    <div className='w-1/2 md:w-1/5 m-4 relative bg-sky-950 overflow-y-hidden shadow-md shadow-black hover:scale-105 duration-200 rounded group'>
         <img src={image_path+movie.info.poster_path} alt='not found' onError={addDefaultImg}
         className='' />
 
-        <div className='p-3 flex flex-col justify-center items-center group cursor-pointer'>
+        <div className='p-3 flex flex-col justify-center items-center md:group cursor-default'>
             <div className='p-2 md:p-3 absolute top-0 right-0 flex items-center rounded-bl-lg bg-sky-950'>
             <FaStar className='fill-yellow-400 mr-1 mt-[2px]'/>
               <p className='text-sm '>{parseFloat(movie.info.vote_average).toFixed(1)}</p>
@@ -23,7 +23,7 @@ const Card = (movie) => {
                 <h1 className='text-md text-center title'>{movie.info.title}</h1>
             </div>
             <div className='mt-2 p-2 absolute bottom-0 left-0 opacity-85 bg-sky-950 overflow-y-auto translate-y-[101%] group-hover:translate-y-0 duration-300'>
-                <h5 >Overview:</h5>
+                <h5 className='text-center '>Overview:</h5>
                 <p className='font-thin text-xs'>{movie.info.overview}</p>
             </div>
         </div>
--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -56,12 +56,13 @@ const Content = () => {
   const handleMenu=()=>{
     setMenu(!menu)
   }
+
   return (
     <>
         <nav className=' h-20 flex justify-between items-center fixed top-0 w-full shadow-md shadow-black bg-[#0f172a] z-10'>
         
         <div className='h-16 flex items-center'>
-            <img src='./assets/logo.png' onClick={() => window.location.reload(false)} className='md:h-14 h-full cursor-pointer ' />
+            <img src='./assets/logo.png' onClick={() => window.location.reload(false)} className='md:h-14 h-full cursor-pointer object-contain' fill />
         </div>
 
         <div className='p-2 '>
@@ -77,9 +78,8 @@ const Content = () => {
         </div>
 
         <div className='relative mr-6'>
-            <form className=''>        
-            <IoMdSearch className='absolute inset-y-[27%] right-2 fill-black cursor-pointer'  onClick={(e) => {searchMovie(e);}}/>         
-            <input type="text" placeholder='Enter movie name' onChange={(e)=>{setSearch(e.target.value)}} value={search}   onKeyDown={(e) => {searchMovie(e);}}
+            <form className=''>                
+            <input type="text" placeholder='Enter movie name' onChange={(e)=>{setSearch(e.target.value)}}  onKeyDown={(e) => {searchMovie(e);}}
              className=' px-4 py-1 rounded-full border-1 border-black text-black font-thin'/>
             </form>
         </div>
@@ -90,13 +90,13 @@ const Content = () => {
 
         <div className={menu?'bg-[#00172D] absolute top-0 right-0 h-screen w-1/2 z-10':'hidden'}>
           <IoMdClose size={24} className=' mx-2 my-5 absolute right-0 cursor-pointer hover:bg-gray-400 duration-200 rounded-full' onClick={handleMenu}/>
-          <ul className='flex flex-col mt-16'>
-          <h1 className=' m-4 text-2xl'>Genres</h1>
+          <h1 className=' m-4 text-3xl mt-20'>Genres</h1>
+          <ul className='flex flex-col mt-6 gap-3'>
             {
 
               arr.map((value,pos)=>{
                   return(
-                      <li className='p-2 mx-2 cursor-pointer rounded underline-offset-8 decoration-2 ative:bg-black duration-150 '><a href="#" className='focus:text-[#38bdf8]' key={pos} name={value} onClick={(e)=>{getData(e.target.name)}}>{value}</a></li>
+                      <li className='p-2 mx-2 cursor-pointer rounded underline-offset-8 decoration-2 ative:bg-black duration-150 ' onClick={handleMenu}><a href="#" className='focus:text-[#38bdf8]' key={pos} name={value} onClick={(e)=>{getData(e.target.name)}}>{value}</a></li>
                   )
               })
             }
